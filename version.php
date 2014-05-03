7f18d4888daf3801344142080d54de823bedb33a
Bas Brands <bas@sonsbeekmedia.nl>
2014-05-03 10:51:20 +0200

    Bootstrap 3 child theme Amelia

---
+++ version.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for amelia details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Theme More version file.
+ *
+ * @package    theme_amelia
+ * @copyright  2014 Bas Brands
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version   = 2014032401;
+$plugin->requires  = 2013110500;
+$plugin->component = 'theme_amelia';
+$plugin->dependencies = array(
+    'theme_bootstrap'  => 2013110500
+);
